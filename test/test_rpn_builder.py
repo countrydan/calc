09a1801c67fd162e6615bc7a050c72a19d068421
Daniel Orszag <orszdani@gmail.com>
2024-02-23 09:23:36 +0100

    add tests

---
+++ test/test_rpn_builder.py
@@ -0,0 +1,30 @@
+import pytest
+
+from main import build_rpn
+
+
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        ("1+2", [1, 2, "+"]),
+        ("1+2-3", [1, 2, "+", 3, "-"]),
+        ("1+(1+(1-2))", [1, 1, 1, 2, "-", "+", "+"]),
+        (
+            "3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3",
+            [3, 4, 2, "*", 1, 5, "-", 2, 3, "^", "^", "/", "+"],
+        ),
+    ],
+)
+def test_build_rpn(test_input, expected):
+    rpn = build_rpn(test_input)
+    assert rpn == expected
+
+
+def test_build_rpn_fail_unknown_token():
+    with pytest.raises(Exception, match="Unknown token: @"):
+        build_rpn("@")
+
+
+def test_build_rpn_fail_dangling_parentheses():
+    with pytest.raises(Exception, match="Dangling left parenthesis"):
+        build_rpn("(1+1")
