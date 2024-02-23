086e46eed92f43d776de3455ef26ea1b4ab799b4
Daniel Orszag <orszdani@gmail.com>
2024-02-23 09:22:59 +0100

    refactor

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import sys
 from enum import Enum, auto
 from typing import NamedTuple
+from collections import deque
 
 input = sys.argv[1]
 
 
 class Associativity(Enum):
-    LEFT = (auto(),)
+    LEFT = auto()
     RIGHT = auto()
 
 
@@ -20,49 +21,59 @@ precedence: dict[str, Operator] = {
     "-": Operator(1, Associativity.LEFT),
     "*": Operator(2, Associativity.LEFT),
     "/": Operator(2, Associativity.LEFT),
-    "^": Operator(2, Associativity.RIGHT),
+    "^": Operator(3, Associativity.RIGHT),
 }
 
-output: list[int | float | str] = []
-operators: list[str] = []
+type RPN = list[int | float | str]
 
-# (1 * 2) - (3 * 4) -> 1 2 * 3 4 * -
-# 1 + (1 + 1) * 5 -> 1 1 1 + 5 * +
-"""
-           there is an operator o2 at the top of the operator stack which is not a left parenthesis, 
-            and (o2 has greater precedence than o1 or (o1 and o2 have the same precedence and o1 is left-associative))"""
-for char in input:
-    match char:
-        case char if char.isnumeric():
-            num = int(char) if "." or "," in char else float(char)  # TODO use result
-            output.append(num)
-        case char if char.isspace():
-            continue
-        case char if char == "(":
-            operators.append(char)
-        case char if char in ("+", "-", "*", "/", "^"):
-            current_operator = precedence.get(char)  # TODO use result
-            top_operator = precedence.get(operators[-1])
-            while operators[-1] != "(" and (
-                top_operator.precedence > current_operator.precedence
-                or (
-                    current_operator.precedence == top_operator.precedence
-                    and current_operator.associativity == Associativity.LEFT
-                )
-            ):
-                output.append(operators.pop())
-                top_operator = precedence.get(operators[-1])
-            operators.append(char)
-        case char if char == ")":
-            while (operator := operators.pop()) != "(":
-                output.append(operator)
-        case _:
-            raise Exception(f"Unknown token: {char}")
 
-if "(" in operators:
-    raise Exception("Dangling left parenthesis")
-output.extend(operators)
-print(output)
+def build_rpn(user_input: str) -> RPN:
+    rpn: RPN = []
+    operators: deque[str] = deque()
+
+    # (1 * 2) - (3 * 4) -> 1 2 * 3 4 * -
+    # 1 + (1 + 1) * 5 -> 1 1 1 + 5 * +
+    """
+               there is an operator o2 at the top of the operator stack which is not a left parenthesis, 
+                and (o2 has greater precedence than o1 or (o1 and o2 have the same precedence and o1 is left-associative))"""
+    for char in user_input:
+        match char:
+            case char if char.isnumeric():
+                num = int(char) if "." or "," in char else float(char)
+                rpn.append(num)
+            case char if char.isspace():
+                continue
+            case char if char == "(":
+                operators.appendleft(char)
+            case char if char in ("+", "-", "*", "/", "^"):
+                if operators:
+                    current_operator = precedence.get(char)
+                    top_operator = precedence.get(operators[0])
+                    while operators[0] != "(" and (
+                        top_operator.precedence > current_operator.precedence
+                        or (
+                            current_operator.precedence == top_operator.precedence
+                            and current_operator.associativity == Associativity.LEFT
+                        )
+                    ):
+                        rpn.append(operators.popleft())
+                        if operators:
+                            top_operator = precedence.get(operators[0])
+                        else:
+                            break
+                operators.appendleft(char)
+            case char if char == ")":
+                while (operator := operators.popleft()) != "(":
+                    rpn.append(operator)
+            case _:
+                raise Exception(f"Unknown token: {char}")
+
+    if "(" in operators:
+        raise Exception("Dangling left parenthesis")
+    rpn.extend(operators)
+    return rpn
+
+
 # if char.isnumeric():
 #     pass
 # else:
